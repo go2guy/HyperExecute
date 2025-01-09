d9ac8f9056bb20b588bd013c2f346c0eea5c2c8c
Herb Scruggs <herb.scruggs@fcsamerica.com>
2025-01-09 08:40:44 -0600

    remote driver

--- Tests/BaseTest.cs
+++ Tests/BaseTest.cs
@@ -1,8 +1,10 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
 using SeleniumInterview.Base;
 using WebDriverManager.DriverConfigs.Impl;
 using WebDriverManager.Helpers;
+
 
 namespace SeleniumInterview.Tests
 {
@@ -15,10 +17,22 @@ namespace SeleniumInterview.Tests
         [OneTimeSetUp]
         public void SetUp()
         {
-            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig(), VersionResolveStrategy.MatchingBrowser);   
-            Driver = new ChromeDriver();
+            //new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig(), VersionResolveStrategy.MatchingBrowser);   
+            //Driver = new ChromeDriver();
+            //Driver.Manage().Window.Maximize();
+            //Driver.Navigate().GoToUrl(_baseUrl);
+
+            var options = new ChromeOptions();
+            options.AddArgument("--start-maximized");
+            var lt_username = "herb.scruggs";
+            var lt_access_key = "dZzoXAahYZcEgODLx6JOnI9qAiNiksiiRNV1Jkxe05qGhl5S8s";
+            var gridURL = "hub.lambdatest.com/wd/hub";
+            
+            Driver = new RemoteWebDriver(new Uri("https://" + lt_username + ":" + lt_access_key + gridURL),
+                            (ICapabilities)options, TimeSpan.FromSeconds(600));
             Driver.Manage().Window.Maximize();
             Driver.Navigate().GoToUrl(_baseUrl);
+
         }
 
         [OneTimeTearDown]
